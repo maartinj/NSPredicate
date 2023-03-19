ad0ce119d7ccef587f0f5be79a3d2187ded4e906
Marcin Jędrzejak <53284922+maartinj@users.noreply.github.com>
2023-03-20 00:17:31 +0100

    Completed NSPredicate for Swift

--- NSPredicate.playground/Pages/Introducing NSPredicate.xcplaygroundpage/Contents.swift
+++ NSPredicate.playground/Pages/Introducing NSPredicate.xcplaygroundpage/Contents.swift
@@ -43,17 +43,64 @@ code(for: "NSPredicate Formats") {
     // Athletes playing a ball game beginning with the letter 'f'
     let fBallAthletes = Athlete.allNSArray
         .filtered(using: NSPredicate(format: "sport.name LIKE[c] 'f*ball'"))
-    dump(fBallAthletes)
+//    dump(fBallAthletes)
     
 }
 
 code(for: "NSPredicate Argments") {
     // Convert all examples above to use NSPredicate arguments
+    // The athlete with the first name "Carson"
+    let name = "Carson"
+    let firstNameIsCarson = Athlete.allNSArray
+        .filtered(using: NSPredicate(format: "firstName == %@", name))
+//    dump(firstNameIsCarson)
+    
+    // All athletes with a first name containing the letter c
+    let letter = "c"
+    let firstNameContainsC = Athlete.allNSArray
+        .filtered(using: NSPredicate(format: "firstName CONTAINS[c] %@", letter))
+//    dump(firstNameContainsC)
+    
+    // All athletes making a salary greater than $100 million per year
+    let highMin = 100
+    let highEarners = Athlete.allNSArray
+        .filtered(using: NSPredicate(format: "sport.salary > %d", highMin))
+//    dump(highEarners)
+    
+    // All athletes earning between $70 and $100 million a year
+    let lowerLimit = 70
+    let higherLimit = 100
+    let midRangeErners = Athlete.allNSArray
+        .filtered(using: NSPredicate(format: "sport.salary BETWEEN {%d, %d}", lowerLimit, higherLimit))
+//    dump(midRangeErners)
+    
+    //--------- More Formats Where NSPredicate Shines ------------------
+    
+    // Athletes with a last name beginning with the letter m
+    let beginLetter = "m"
+    let lastNameBeginsWithM = Athlete.allNSArray
+        .filtered(using: NSPredicate(format: "lastName BEGINSWITH[c] %@", beginLetter))
+//    dump(lastNameBeginsWithM)
+    
+    // Athletes playing some kind of 'ball' sport
+    let ballGamers = "*ball"
+    let ballGameAthletes = Athlete.allNSArray
+        .filtered(using: NSPredicate(format: "sport.name LIKE[c] %@", ballGamers))
+//    dump(ballGameAthletes)
+    
+    // Athletes playing a ball game beginning with the letter 'f'
+    let fBallers = "f*ball"
+    let fBallAthletes = Athlete.allNSArray
+        .filtered(using: NSPredicate(format: "sport.name LIKE[c] %@", fBallers))
+//    dump(fBallAthletes)
 }
 
 code(for: "Comparison Predicate Options") {
 // Athletes with 'junior' as a suffix for their last name
-
+    let name = "junior"
+    let athletsWithName = Athlete.allNSArray
+        .filtered(using: NSPredicate(format: "lastName CONTAINS[cd] %@", name))
+    dump(athletsWithName)
 }
 
 /*:
